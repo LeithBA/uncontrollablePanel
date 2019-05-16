9d52423b32144fe2e78fa9c58b885b72b4319652
LeithBA <leith.benabdessalem@ue-germany.de>
2019-05-16 14:40:45 +0200

    Leds added, leds and gauges animated, pixel perfect cam added, background tweak

---
+++ Assets/Scripts/GaugeNeedle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GaugeNeedle : MonoBehaviour
+{
+
+    [HideInInspector, Range(0.0f, 1.0f)]public float value = 0.5f;
+    private float rotAngle;
+
+    void Update()
+    {
+		value = Mathf.Clamp(value + Random.Range(-0.01f, 0.01f), 0.0f, 1.0f);
+
+        Map(value, 0.0f, 1.0f, -45, 45);
+		this.transform.rotation =  Quaternion.Euler(0, 0, -rotAngle);
+		Debug.Log(value + " - " + rotAngle);
+    }
+
+    void Map(float value, float from1, float to1, float from2, float to2)
+    {
+		rotAngle = (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+
+}
---
+++ Assets/Scripts/Led.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Led : MonoBehaviour {
+
+	[Range(0.0f, 1.0f), SerializeField] float hue, saturation, brightness;
+
+	void Start()
+	{
+		hue = Random.Range(-0.0f, 1.0f);
+	}
+	void Update () 
+	{
+		hue = hue + Random.Range(-0.01f, 0.01f);
+		if (hue < 0)
+			hue++;
+		else if (hue > 1)
+			hue--;
+		this.GetComponent<SpriteRenderer>().color = Color.HSVToRGB(hue, saturation, brightness);
+	}
+}
